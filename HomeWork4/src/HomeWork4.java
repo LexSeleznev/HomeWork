ca0fccadf19dc2164355e85473b3704d3615f1b6
lex_seleznev <lex.seleznev91@gmail.com>
2022-07-20 19:15:31 +0300

    Final Version

---
+++ HomeWork4/src/HomeWork4.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class HomeWork4 {
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int present = 0;
+        int orange = 0;
+        int apple = 0;
+        int pear = 0;
+        present = sc.nextInt();
+        orange = sc.nextInt();
+        apple = sc.nextInt();
+        pear = sc.nextInt();
+
+    }
+}
--- Homework1/lesson1/src/Main.java
+++ Homework1/lesson1/src/Main.java
@@ -1,52 +1,73 @@
 import java.util.Scanner;
 
 public class Main {
+    static Scanner sc = new Scanner(System.in);
+
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        double num1 = 0;
-        double num2 = 0;
-        String action;
-        double result = 0;
         while (true) {
-            System.out.println("Выберите действие + - * / или введите stop для остановки");
-            action = sc.next();
+            double num1 = getNum();
+            double num2 = getNum();
+            String action = getAction();
+            double result;
             if (action.equals("stop")) {
                 System.out.println("Остановка");
                 break;
+            } else {
+                result = score(num1, num2, action);
             }
-            System.out.println("Введите первое число");
-            num1 = sc.nextDouble();
-            System.out.println("Введите второе число");
-            num2 = sc.nextDouble();
-            switch (action) {
-                case "+":
-                    result = num1 + num2;
-                    System.out.println(num1 + " " + action + " " + num2 + " равно " + result);
-                    result = 0;
-                    break;
-                case "-":
-                    result = num1 - num2;
-                    System.out.println(num1 + " " + action + " " + num2 + " равно " + result);
-                    result = 0;
-                    break;
-                case "/":
-                    while (num2 == 0){
-                        System.out.println("Пирожочек, на ноль делить нельзя, выбери другое число");
-                        num2 = sc.nextDouble();
-                    }
-                    result = num1 / num2;
-                    System.out.println(num1 + " " + action + " " + num2 + " равно " + result);
-                    result = 0;
-                    break;
-                case "*":
-                    result = num1 * num2;
-                    System.out.println(num1 + " " + action + " " + num2 + " равно " + result);
-                    result = 0;
-                    break;
-                default:
-                    System.out.println("Введено неверное действие");
-                    break;
+            if (action.equals("/") && num2 == 0) {
+                System.out.println("Пирожочек, на ноль делить нельзя, давай по новой");
+            }
+            else if (action.equals("-") || action.equals("+") || action.equals("/") || action.equals("*")) {
+                System.out.println(num1 + " " + action + " " + num2 + " = " + result);
+            }
+            else {
+                System.out.println("Введено неверное действие");
             }
         }
+    }
+    public static double getNum() {
+        System.out.println("Введите число");
+        double num;
+        if (sc.hasNextDouble()) {
+            num = sc.nextDouble();
+        } else {
+            System.out.println("Вы ввели не число, попробуйте еще раз");
+            num = getNum();
+        }
+        return num;
+    }
+    public static String getAction() {
+        String action;
+        System.out.println("Выберите действие + - * / или введите stop для остановки");
+        if (sc.hasNext()) {
+            action = sc.next();
+        }
+        else {
+            System.out.println("Вы ввели какую то дичь, выберите операцию из предложенных");
+            action = getAction();
+        }
+        return action;
+    }
+
+    public static double score(double num1, double num2, String action) {
+        double result = 0;
+        switch (action) {
+            case "+":
+                result = num1 + num2;
+                break;
+            case "-":
+                result = num1 - num2;
+                break;
+            case "/":
+                result = num1 / num2;
+                break;
+            case "*":
+                result = num1 * num2;
+                break;
+            default:
+                break;
+        }
+        return result;
     }
 }
