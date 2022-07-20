47b029475a03dadf009ef92a6a3d768f617fc344
lex_seleznev <lex.seleznev91@gmail.com>
2022-07-20 22:41:20 +0300

    0.9 version

--- HomeWork4/src/HomeWork4.java
+++ HomeWork4/src/HomeWork4.java
@@ -1,3 +1,4 @@
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class HomeWork4 {
@@ -7,10 +8,41 @@ public class HomeWork4 {
         int orange = 0;
         int apple = 0;
         int pear = 0;
+        System.out.println("Введите вес подарка");
         present = sc.nextInt();
+        System.out.println("Введите вес апельсинов");
         orange = sc.nextInt();
+        System.out.println("Введите вес яблок");
         apple = sc.nextInt();
+        System.out.println("Введите вес груш");
         pear = sc.nextInt();
 
+        int count = 0;
+        for (int a = 0; a <= present; a = a + orange) {
+            if (a+orange == 0){
+//                System.out.println("A не увеличивается");
+                break;
+            }
+//            System.out.println("a= " + a);
+            for (int b = 0; b <= present; b = b + apple) {
+                if (b+apple == 0){
+ //                   System.out.println("B не увеличивается");
+                    break;
+                }
+//                System.out.println("b= "+ b);
+                for (int c = 0; c <= present; c = c + pear) {
+                    if (c+pear == 0){
+//                        System.out.println("C не увеличивается");
+                        break;
+                    }
+//                    System.out.println("c= " + c);
+                    if (a + b + c == present) {
+                        count++;
+                    }
+                }
+            }
+        }
+        System.out.println("Возможных вариантов " + count);
     }
 }
+
