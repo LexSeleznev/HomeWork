c5e2aaa8cc3434879585724597ff41596abdae4f
lex_seleznev <lex.seleznev91@gmail.com>
2022-07-21 20:57:33 +0300

    0.9 version

--- HomeWork4/src/HomeWork4.java
+++ HomeWork4/src/HomeWork4.java
@@ -1,37 +1,20 @@
-import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class HomeWork4 {
+    static Scanner sc = new Scanner(System.in);
     public static void main (String[] args) {
-        Scanner sc = new Scanner(System.in);
         int present = 0;
         int orange = 0;
         int apple = 0;
         int pear = 0;
         System.out.println("Введите вес подарка");
-        present = sc.nextInt();
+        present = getWeight();
         System.out.println("Введите вес апельсинов");
-        orange = sc.nextInt();
+        orange = getWeight();
         System.out.println("Введите вес яблок");
-        apple = sc.nextInt();
+        apple = getWeight();
         System.out.println("Введите вес груш");
-        pear = sc.nextInt();
-        while (orange == 0) {
-            System.out.println("Апельсин не может быть невесомым, выберите вес апельсина");
-            orange = sc.nextInt();
-        }
-        while (apple == 0) {
-            System.out.println("Яблоко не может быть невесомым, выберите вес яблока");
-            apple = sc.nextInt();
-        }
-        while (pear == 0) {
-            System.out.println("Груша не может быть невесомой, выберите вес груши");
-            pear = sc.nextInt();
-        }
-        while (present == 0) {
-            System.out.println("Подарок не может быть невесомым, выберите вес подарка");
-            present = sc.nextInt();
-        }
+        pear = getWeight();
         int count = 0;
         for (int a = 0; a <= present; a = a + orange) {
             for (int b = 0; b <= present; b = b + apple) {
@@ -43,6 +26,15 @@ public class HomeWork4 {
             }
         }
         System.out.println("Возможных вариантов " + count);
+    }
+    public static int getWeight(int zero) {
+        zero = sc.nextInt();
+        while (zero == 0) {
+            System.out.println("Вы ввели ноль, укажите правильный вес");
+            zero = sc.nextInt();
+
+        }
+        return zero;
     }
 }
 
