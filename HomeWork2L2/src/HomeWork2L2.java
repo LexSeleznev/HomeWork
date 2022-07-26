91a6742c02e1ff71c949794b2b14479412b0dcab
lex_seleznev <lex.seleznev91@gmail.com>
2022-07-26 19:18:47 +0300

    first edition

--- HomeWork2L2/src/HomeWork2L2.java
+++ HomeWork2L2/src/HomeWork2L2.java
@@ -15,12 +15,12 @@ public class HomeWork2L2 {
     }
     public static boolean palindromCheck (String word) {
         String a1 = word;
-        a1 = a1.toLowerCase().replaceAll("\\W","");
+        a1 = a1.toUpperCase().replaceAll("[\\s|\\u00A0]+","");
         StringBuffer a2 = new StringBuffer(a1).reverse();
         String a3 = a2.toString();
         boolean result = false;
         System.out.println("a1 = " + a1);
-        System.out.println("a2 = " + a3);
+        System.out.println("a3 = " + a3);
         if (a1.equals(a3)) {
             result = true;
         }
