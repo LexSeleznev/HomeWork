ce6537fe1569dc951f3f49bde82605b9f1eedf65
lex_seleznev <lex.seleznev91@gmail.com>
2022-07-28 20:50:40 +0300

    1

---
+++ HomeWork3L2/src/HomeWork3L2.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class HomeWork3L2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String num = sc.next();
+        int counter1 = 1;
+        int counter2 = 0;
+        for(int i = 0; i<num.length()-1; i++) {
+            if (num.charAt(i) == num.charAt(i+1)) {
+                counter1++;
+            }
+            else {
+                counter2 = counter1;
+                counter1 = 1;
+            }
+        }
+        System.out.println("Максимаальная последовательность " + counter2);
+    }
+}
